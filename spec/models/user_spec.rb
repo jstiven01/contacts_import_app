85b8d05373d73156dfc39f1beac7c644406c1a2d
jstiven01 <stiven10@gmail.com>
2020-05-15 11:56:50 -0500

    Adding Test validations and setting FactoryBot

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  describe 'validations model' do
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:password) }
+    it 'has a valid factory' do
+      user = FactoryBot.create(:user)
+      expect(user).to be_valid
+    end
+  end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,10 @@ require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
+require 'shoulda/matchers'
+require 'factory_bot_rails'
+require 'devise'
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -69,6 +73,15 @@ RSpec.configure do |config|
   config.around(:each) do |example|
     DatabaseCleaner.cleaning do
       example.run
+    end
+  end
+
+  config.include Devise::Test::IntegrationHelpers, type: :feature
+
+  Shoulda::Matchers.configure do |config_shoulda|
+    config_shoulda.integrate do |with|
+      with.test_framework :rspec
+      with.library :rails
     end
   end
 end
