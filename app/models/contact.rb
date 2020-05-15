eb1315a531160a00ba72c16a21f37e86f27d134c
jstiven01 <stiven10@gmail.com>
2020-05-15 12:55:53 -0500

    Adding name validations to contact

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,4 +2,7 @@ class Contact < ApplicationRecord
   belongs_to :user
   validates_presence_of :name, :birth_date, :phone, :address, :credit_card,
                         :franchise_credit_card, :email, :is_imported
+
+  VALID_NAME_PATTERN = /\A[a-zA-Z\s\d\-]+\z/.freeze
+  validates :name, format: { with: VALID_NAME_PATTERN, message: 'special characters are not allowed except -' }
 end
--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Contact, type: :model do
-  describe 'validations model' do
+  describe 'presence validations model and Factory Bot' do
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:birth_date) }
     it { should validate_presence_of(:phone) }
@@ -15,5 +15,16 @@ RSpec.describe Contact, type: :model do
       contact = FactoryBot.create(:contact)
       expect(contact).to be_valid
     end
+  end
+
+  describe 'associations test' do
+    it { is_expected.to belong_to(:user) }
+  end
+
+  describe 'format name validations' do
+    it { should allow_value('John Doe').for(:name) }
+    it { should allow_value('John-Doe').for(:name) }
+    it { should allow_value('John Doe1').for(:name) }
+    it { should_not allow_value('John ? Doe').for(:name).with_message('special characters are not allowed except -') }
   end
 end
