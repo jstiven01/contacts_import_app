28b727954f633766927ff711c14468430b81b21c
jstiven01 <stiven10@gmail.com>
2020-05-15 15:44:01 -0500

    Testing signup feature

---
+++ spec/features/user_sign_up_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe 'User sign up', type: :feature do
+  describe 'User sign up cases' do
+    before(:each) do
+      visit root_path
+      click_link 'Sign up'
+      within 'form' do
+        fill_in 'Email', with: 'kingLear@tragedy.com'
+        fill_in 'Password', with: 'aaa111'
+        fill_in 'Password confirmation', with: 'aaa111'
+      end
+    end
+
+    scenario 'User signs up successfully' do
+      click_button 'Sign up'
+      expect(page).to have_content('Welcome! You have signed up successfully.')
+    end
+  end
+end
