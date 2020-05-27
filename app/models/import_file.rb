b97e551a4fe088856ef17c851f0585cba85e6a74
Johan Tinjaca <stiven10@gmail.com>
2020-05-27 14:46:58 -0500

    Refactoring import file

--- app/models/import_file.rb
+++ app/models/import_file.rb
@@ -10,7 +10,7 @@ class ImportFile < ApplicationRecord
     state :waiting, :error, :complete
 
     event :success_upload do
-      transitions from: :processing, to: :complete
+      transitions from: %i[processing error], to: :complete
     end
 
     event :fail_upload do
@@ -20,35 +20,34 @@ class ImportFile < ApplicationRecord
 
   def import_data(params_form)
     file = params_form[:file]
-    error_importing = true
     CSV.foreach(file.path, headers: true) do |row|
-      #p 'new contact', processing_columns(params_form, row.to_hash)
       new_contact = user.contacts.build processing_columns(params_form, row.to_hash)
       if new_contact.save
-        error_importing = false
+        success_upload! if may_success_upload?
       else
-        new_invalid_contact = user.invalid_contacts.build processing_columns(params_form, row.to_hash)
-        new_invalid_contact.error_desc = new_contact.errors.full_messages.first
-        new_invalid_contact.save
+        saving_invalid_contact(params_form, new_contact, row)
+        fail_upload! if may_fail_upload?
       end
-    end
-    if error_importing
-      fail_upload!
-    else
-      success_upload!
     end
   end
 
-  def processing_columns(params_form, row_hash)
-    hash_relation = {
-      params_form[:name].to_s => 'name',
-      params_form[:birth_date].to_s => 'birth_date',
-      params_form[:phone].to_s => 'phone',
-      params_form[:address].to_s => 'address',
-      params_form[:credit_card].to_s => 'credit_card',
-      params_form[:email].to_s => 'email'
+  def saving_invalid_contact(params_form, new_contact, row_contact)
+    new_invalid_contact = user.invalid_contacts.build processing_columns(params_form, row_contact.to_hash)
+    new_invalid_contact.error_desc = new_contact.errors.full_messages.first
+    new_invalid_contact.save
+  end
+
+  def mapping_fields_file(params_form)
+    {
+      params_form[:name].to_s => 'name', params_form[:birth_date].to_s => 'birth_date',
+      params_form[:phone].to_s => 'phone', params_form[:address].to_s => 'address',
+      params_form[:credit_card].to_s => 'credit_card', params_form[:email].to_s => 'email'
     }
+  end
+
+  def processing_columns(params_form, row_hash)
     final_hash = {}
+    hash_relation = mapping_fields_file(params_form)
     row_hash.each do |key, element|
       final_hash[hash_relation[key]] = element if hash_relation.keys.include?(key)
     end
